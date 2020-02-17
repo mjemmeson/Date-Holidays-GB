08709a830da49a067afd60e6696a611dcbb1b08e
Michael Jemmeson <mjemmeson@cpan.org>
2020-02-17 14:08:10 +0000

    correct date in abstract

--- lib/Date/Holidays/GB.pm
+++ lib/Date/Holidays/GB.pm
@@ -2,7 +2,7 @@ package Date::Holidays::GB;
 
 our $VERSION = '0.014';
 
-# ABSTRACT: Determine British holidays - Current UK public and bank holiday dates up to 2019
+# ABSTRACT: Determine British holidays - Current UK public and bank holiday dates up to 2021
 
 use strict;
 use warnings;
